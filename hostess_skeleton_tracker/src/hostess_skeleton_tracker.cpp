dba39ab7d33e6afdc193c830915f7700482592e3
Giuseppe <giuseppecarboni89@live.com>
2016-01-20 19:45:51 +0100

    commit

--- hostess_skeleton_tracker/src/hostess_skeleton_tracker.cpp
+++ hostess_skeleton_tracker/src/hostess_skeleton_tracker.cpp
@@ -9,8 +9,10 @@
 #include <XnCodecIDs.h>
 #include <XnCppWrapper.h>
 #include <map>
+#include <opencv2/video/tracking.hpp>
 
 #define MAX_USERS 6
+#define DISTANCE_THRESHOLD 0.2
 
 std::string genericUserCalibrationFileName;
 
@@ -27,13 +29,34 @@ void XN_CALLBACK_TYPE User_BackIntoScene(xn::UserGenerator&, XnUserID, void*);
 
 XnUInt32 calibrationData;
 
-void publishTransform(XnUserID const&, XnSkeletonJoint const&, std::string const&, std::string const&);
-void publishHeadTransforms(const std::string&);
-void publishTorsoTransform(const std::string&, int&);
+void publishTransform(tf::TransformBroadcaster&, XnUserID const&, XnSkeletonJoint const&, std::string const&, std::string const&);
+void publishHeadTransforms(tf::TransformBroadcaster&, const std::string&);
+bool publishTorsoTransform(tf::TransformBroadcaster&, const std::string&, int);
+void publishAllTorsos(tf::TransformBroadcaster&, const std::string&);
 bool checkCenterOfMass(XnUserID const&);
 void stopTrackingAll(int);
 void startTrackingAll();
-void kalmanizeTorso(tf::TransformListener&, int);
+void predictAndPublish(tf::TransformBroadcaster&);
+void kalmanPrediction();
+void kalmanUpdate(tf::TransformListener&, int);
+int checkBestMatch(tf::TransformBroadcaster&, tf::TransformListener&);
+
+//--------------Kalman Filter-----------------
+
+const int stateSize = 6;
+const int measSize = 3;
+const int contrSize = 0;
+cv::KalmanFilter kf(stateSize, measSize, contrSize);
+
+cv::Mat state(stateSize, 1, CV_32F);
+cv::Mat meas(measSize, 1, CV_32F);
+
+double ticks = 0;
+int notFoundCount = 0;
+bool detected = false;
+bool newMeas = false;
+
+ros::Time lastStamp;
 
 int main(int argc, char **argv)
 {
@@ -106,7 +129,21 @@ int main(int argc, char **argv)
     int skeleton_to_track = 0;
     nh.setParam("skeleton_to_track", skeleton_to_track);
 
-    tf::TransformListener listener;
+    cv::setIdentity(kf.transitionMatrix);
+    cv::setIdentity(kf.errorCovPre);
+
+	kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, CV_32F);
+	kf.measurementMatrix.at<float>(0) = 1.0f;
+	kf.measurementMatrix.at<float>(7) = 1.0f;
+	kf.measurementMatrix.at<float>(14) = 1.0f;
+
+	cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
+	cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+
+	static tf::TransformListener listener;
+	static tf::TransformBroadcaster br;
+
+	ros::Duration timeout;
 
 	while(nh.ok())
 	{
@@ -118,30 +155,71 @@ int main(int argc, char **argv)
 			}
 
 			g_Context.WaitAndUpdateAll();
-			publishHeadTransforms(frame_id);
+			publishHeadTransforms(br, frame_id);
 
 			ros::Rate(30).sleep();
 		}
 
-		stopTrackingAll(skeleton_to_track);
-
-		while(nh.ok())
+		while(nh.ok() && skeleton_to_track != 0)
 		{
-			g_Context.WaitAndUpdateAll();
-			publishTorsoTransform(frame_id, skeleton_to_track);
+			stopTrackingAll(skeleton_to_track);
 
-			kalmanizeTorso(listener, skeleton_to_track);
-
-			ros::Rate(30).sleep();
-
-			if(skeleton_to_track == 0)
+			while(nh.ok())
 			{
-				ros::param::set("skeleton_to_track", skeleton_to_track);
-				break;
+				if(detected)
+				{
+					predictAndPublish(br);
+				}
+
+				g_Context.WaitAndUpdateAll();
+
+				if(!publishTorsoTransform(br, frame_id, skeleton_to_track))
+				{
+					break;
+				}
+				else
+				{
+					kalmanUpdate(listener, skeleton_to_track);
+					ros::Rate(30).sleep();
+				}
+			}
+
+			startTrackingAll();
+
+			ros::Time before = ros::Time::now();
+
+			timeout.sec = timeout.nsec = 0;
+
+			while(nh.ok())
+			{
+				g_Context.WaitAndUpdateAll();
+
+				publishAllTorsos(br, frame_id);
+
+				int index = checkBestMatch(br, listener);
+
+				if(index != 0)
+				{
+					skeleton_to_track = index;
+					break;
+				}
+				else
+				{
+					timeout += ros::Time::now() - before;
+					before = ros::Time::now();
+
+					if(timeout.sec > 2)
+					{
+						skeleton_to_track = 0;
+						break;
+					}
+				}
+
+				ros::Rate(30).sleep();
 			}
 		}
 
-		startTrackingAll();
+		ros::param::set("skeleton_to_track", skeleton_to_track);
 	}
 
 	g_Context.Shutdown();
@@ -199,10 +277,8 @@ void XN_CALLBACK_TYPE User_OutOfScene(xn::UserGenerator& generator, XnUserID nId
 	}
 }
 
-void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, std::string const& frame_id, std::string const& child_frame_id)
+void publishTransform(tf::TransformBroadcaster& br, XnUserID const& user, XnSkeletonJoint const& joint, std::string const& frame_id, std::string const& child_frame_id)
 {
-    static tf::TransformBroadcaster br;
-
     XnSkeletonJointPosition joint_position;
     XnSkeletonJointOrientation joint_orientation;
 
@@ -241,11 +317,9 @@ void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, std::s
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_no));
 }
 
-void publishHeadTransforms(const std::string& frame_id)
+void publishHeadTransforms(tf::TransformBroadcaster& br, const std::string& frame_id)
 {
-    ros::Time now = ros::Time::now();
-
-    XnUInt16 users_count = MAX_USERS;
+	XnUInt16 users_count = MAX_USERS;
     XnUserID users[MAX_USERS];
 
     g_UserGenerator.GetUsers(users, users_count);
@@ -259,26 +333,85 @@ void publishHeadTransforms(const std::string& frame_id)
             continue;
         }
 
-        publishTransform(user, XN_SKEL_HEAD, frame_id, "head");
+        publishTransform(br, user, XN_SKEL_HEAD, frame_id, "head");
     }
 }
 
-void publishTorsoTransform(const std::string& frame_id, int& skeleton_to_track)
+bool publishTorsoTransform(tf::TransformBroadcaster& br, const std::string& frame_id, int skeleton_to_track)
 {
-    ros::Time now = ros::Time::now();
-
-	if(checkCenterOfMass(skeleton_to_track) && g_UserGenerator.GetSkeletonCap().IsTracking(skeleton_to_track))
+    if(checkCenterOfMass(skeleton_to_track) && g_UserGenerator.GetSkeletonCap().IsTracking(skeleton_to_track))
 	{
-		publishTransform(skeleton_to_track, XN_SKEL_TORSO, frame_id, "torso");
+		publishTransform(br, skeleton_to_track, XN_SKEL_TORSO, frame_id, "torso");
+		return true;
 	}
 	else
 	{
-		skeleton_to_track = 0;
+		return false;
 	}
 }
 
-void kalmanizeTorso(tf::TransformListener& listener, int skeleton_to_track)
+void publishAllTorsos(tf::TransformBroadcaster& br, const std::string& frame_id)
 {
+	ROS_INFO("Publishing all torsos");
+
+	XnUInt16 users_count = MAX_USERS;
+	XnUserID users[MAX_USERS];
+
+	g_UserGenerator.GetUsers(users, users_count);
+
+	for(int i = 0; i < users_count; ++i)
+	{
+		XnUserID user = users[i];
+
+		if(!checkCenterOfMass(user))
+		{
+			continue;
+		}
+
+		publishTransform(br, user, XN_SKEL_TORSO, frame_id, "torso");
+	}
+
+	ROS_INFO("Published all torsos");
+}
+
+void predictAndPublish(tf::TransformBroadcaster& br)
+{
+	kalmanPrediction();
+
+	double x = state.at<float>(0);
+	double y = state.at<float>(1);
+	double z = state.at<float>(2);
+
+	tf::Transform transform;
+	transform.setOrigin(tf::Vector3(x, y, z));
+	transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+
+	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "torso_k"));
+}
+
+void kalmanPrediction()
+{
+	ROS_INFO("Kalman prediction started");
+
+	double precTick = ticks;
+
+	ticks = (double)cv::getTickCount();
+
+	double dT = (ticks - precTick) / cv::getTickFrequency();
+
+	kf.transitionMatrix.at<float>(2) = dT;
+	kf.transitionMatrix.at<float>(9) = dT;
+	kf.transitionMatrix.at<float>(16) = dT;
+
+	state = kf.predict();
+
+	ROS_INFO("Kalman prediction completed");
+}
+
+void kalmanUpdate(tf::TransformListener& listener, int skeleton_to_track)
+{
+	ROS_INFO("Kalman update started");
+
 	tf::StampedTransform transform;
 
 	std::ostringstream oss;
@@ -288,11 +421,93 @@ void kalmanizeTorso(tf::TransformListener& listener, int skeleton_to_track)
 	{
 		listener.lookupTransform("map", oss.str(), ros::Time(0), transform);
 
-		std::cout << "I heard TF" << std::endl;
+		if(lastStamp == transform.stamp_)
+		{
+			return;
+		}
+		else
+		{
+			lastStamp = transform.stamp_;
+
+			notFoundCount = 0;
+
+			if(!detected)
+			{
+				detected = true;
+
+				state.at<float>(0) = transform.getOrigin().getX();
+				state.at<float>(1) = transform.getOrigin().getY();
+				state.at<float>(2) = transform.getOrigin().getZ();
+				state.at<float>(3) = 0;
+				state.at<float>(4) = 0;
+				state.at<float>(5) = 0;
+
+				kf.statePost = state;
+			}
+			else
+			{
+				meas.at<float>(0) = transform.getOrigin().getX();
+				meas.at<float>(1) = transform.getOrigin().getY();
+				meas.at<float>(2) = transform.getOrigin().getZ();
+
+				kf.correct(meas);
+			}
+		}
 	}
 	catch(tf::TransformException &ex)
 	{
-		std::cout << "I heard TF" << std::endl;
+
+	}
+
+	ROS_INFO("Kalman update completed");
+}
+
+int checkBestMatch(tf::TransformBroadcaster& br, tf::TransformListener& listener)
+{
+	predictAndPublish(br);
+
+	double min = std::numeric_limits<double>::max();
+	std::string frame_to_track;
+
+	XnUInt16 users_count = MAX_USERS;
+	XnUserID users[MAX_USERS];
+
+	g_UserGenerator.GetUsers(users, users_count);
+
+	for(int i = 0; i < users_count; ++i)
+	{
+		tf::StampedTransform transform;
+
+		std::ostringstream oss;
+		oss << "torso_" << i;
+
+		try
+		{
+			listener.lookupTransform("map", oss.str(), ros::Time::now(), transform);
+
+			double current = std::sqrt(std::pow(transform.getOrigin().getX(), 2) + std::pow(transform.getOrigin().getY(), 2) + std::pow(transform.getOrigin().getZ(), 2));
+
+			if(current < min)
+			{
+				min = current;
+				frame_to_track = transform.child_frame_id_;
+			}
+		}
+		catch(tf::TransformException &ex)
+		{
+
+		}
+	}
+
+	if(min < DISTANCE_THRESHOLD)
+	{
+		//RIASSOCIAZIONE UTENTE
+
+		return atoi(frame_to_track.substr(frame_to_track.find("_") + 1, frame_to_track.size()).c_str());
+	}
+	else
+	{
+		return 0;
 	}
 }
 
